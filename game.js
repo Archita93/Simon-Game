f44c62e14597d9f2d8e3632c050dfaa9bd82ff6d
Archita <asa410@sfu.ca>
2022-12-27 08:30:11 -0800

    Used an event handler to detect the button clicked by the user

--- game.js
+++ game.js
@@ -2,17 +2,32 @@ var buttonColours = ["red","blue","green","yellow"];
 
 var gamePattern = [];
 
+var userClickedPattern = [];
+
 var audio;
 function nextSequence(){
 
     var  randomNumber = Math.floor(Math.random()*4);
     var randomChosenColour = buttonColours[randomNumber];
-    // gamePattern.push(randomChosenColour);
-    // var song = randomChosenColour + ".mp3"
-    // console.log(song);
+
+    // Pushed to gamePattern array
+    gamePattern.push(randomChosenColour);
+    
+    // Added an audio for chosen button
     audio = new Audio("sounds/"+randomChosenColour+".mp3");
+
+    // Added animation for the chosen button
     $("#"+randomChosenColour).fadeOut(100).fadeIn(100);
+
+    // Played the audio 
     audio.play();
+
+    // returned the chosen colour
     return randomChosenColour;
 }
 
+$(".btn").click(function(){
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    console.log(userClickedPattern);
+})
